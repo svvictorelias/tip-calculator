cbfee4ec97d0c6032ed8407ca1507501b4256981
Victor Elias <svvictorelias@gmail.com>
2022-04-22 22:40:26 -0300

    adicionado função construtora para realização das contas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,23 @@
-console.log('oi')
+function Tip(){
+    this.bill = document.querySelector('#bill')
+    this.start = () =>{
+        this.catchClick()
+    }
+    this.catchClick = () =>{
+        document.addEventListener('click',e=>{
+            const el = e.target
+            if(el.classList.contains('quinzePorcento')) this.quinzePorcento()
+        })
+    }
+    this.realizaConta = porcento =>{
+        let billText = this.bill.value
+        let billConta = billText*porcento
+        console.log(billConta)
+    }
+    this.quinzePorcento = () =>{
+        this.realizaConta(0.15)
+    }
+
+}
+const tip =  new Tip()
+tip.start()
